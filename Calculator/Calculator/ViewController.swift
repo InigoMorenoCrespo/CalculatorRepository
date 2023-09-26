66bcc3597625eaaef52465c35bbee6896a2d1e66
Iñigo moreno Crespo <inigo.morenocrespo@plexus.tech>
2023-09-26 15:48:24 +0200

    empty app

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Iñigo Moreno Crespo on 26/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
